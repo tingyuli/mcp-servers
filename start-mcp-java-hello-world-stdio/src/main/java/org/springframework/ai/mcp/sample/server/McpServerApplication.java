5bdf1819e1765b125787b705c5445b74781a4a9b
Zone Tome <e0983175@u.nus.edu>
2025-04-09 02:48:49 +0800

    Reapply "feat: 添加 Spring Boot MCP Weather STDIO 服务器示例"
    This reverts commit 7b2b6c4ad423dbba05102d7ee75022e341bd30bb.

---
+++ start-mcp-java-hello-world-stdio/src/main/java/org/springframework/ai/mcp/sample/server/McpServerApplication.java
@@ -0,0 +1,24 @@
+package org.springframework.ai.mcp.sample.server;
+
+import org.springframework.ai.tool.ToolCallbackProvider;
+import org.springframework.ai.tool.method.MethodToolCallbackProvider;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class McpServerApplication {
+
+	public static void main(String[] args) {
+		// Start the Spring Boot application
+		// and the MCP server
+		System.out.println("Starting MCP server...");
+		SpringApplication.run(McpServerApplication.class, args);
+	}
+
+	@Bean
+	public ToolCallbackProvider weatherTools(WeatherService weatherService) {
+		return MethodToolCallbackProvider.builder().toolObjects(weatherService).build();
+	}
+
+}
